cea95c9bcea81bf432070336e18469a0c4cc9415
cordialgreetings <hr2579023575@163.com>
2021-07-28 19:26:56 +0800

    v2:MVC initial

---
+++ v2/src/main/java/com/example/v2/Chatroom.java
@@ -0,0 +1,13 @@
+package com.example.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatroom {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chatroom.class, args);
+    }
+
+}
---
+++ v2/src/test/java/com/example/v2/ChatroomTests.java
@@ -0,0 +1,13 @@
+package com.example.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
