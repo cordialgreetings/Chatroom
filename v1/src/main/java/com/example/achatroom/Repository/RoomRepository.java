f4f2b5307c3f38d37acbd4219d6da01f623452e5
cordialgreetings <hr2579023575@163.com>
2021-08-04 13:28:32 +0800

    v1:leave room

--- v1/src/main/java/com/example/achatroom/Repository/RoomRepository.java
+++ v1/src/main/java/com/example/achatroom/Repository/RoomRepository.java
@@ -28,6 +28,7 @@ public class RoomRepository {
     public static final String GET_ROOM_INFO_SQL = "select `name` from `room` where roomId=?";
     public static final String GET_USERS_SQL = "select `username` from `roomuser` where roomId=?";
     public static final String GET_ROOMS_SQL = "select * from `room` where ?<roomId limit ?";
+
     @Autowired
     public RoomRepository(Mono<Connection> connectionMysql, ReactiveStringRedisTemplate reactiveStringRedisTemplate, KeyGenerator keyGenerator) {
         this.connectionMysql = connectionMysql;
@@ -35,43 +36,43 @@ public class RoomRepository {
         this.keyGenerator = keyGenerator;
     }
 
-    public Mono<String> createRoom(String name){
+    public Mono<String> createRoom(String name) {
         return connectionMysql.flatMap(connection ->
                 Mono.from(connection.createStatement(CREATE_ROOM_SQL)
-                        .bind(0, keyGenerator.generate()).bind(1,name)
+                        .bind(0, keyGenerator.generate()).bind(1, name)
                         .returnGeneratedValues("roomId").execute())
                         .flatMap(result -> Mono.from(result.map((row, rowMetadata) -> row.get(0, Integer.class))))
                         .map(Object::toString)
-                        .doFinally(signalType -> ((Mono<Void>)connection.close()).subscribe()));
+                        .doFinally(signalType -> ((Mono<Void>) connection.close()).subscribe()));
     }
 
-    public Mono<Boolean> enterRoom(String username, int roomId){
+    public Mono<Boolean> enterRoom(String username, int roomId) {
         int max_id = keyGenerator.get();
-        if(roomId<=0||roomId>max_id){
+        if (roomId <= 0 || roomId > max_id) {
             return falseMono;
         }
-         return redisTemplate.opsForHash().get(USER2ROOM, username)
-                 .switchIfEmpty(initRoomIdMono)
-                 .map(i->Integer.parseInt((String) i,10))
-                 .flatMap(oldRoomId->{
-                     if(oldRoomId==roomId){
-                         return trueMono;
-                     }
-                     Mono<?> afterMysql;
-                     if(oldRoomId!=-1){
-                         afterMysql = connectionMysql.flatMap(connection ->
-                                 ((Mono<Void>)connection.beginTransaction())
-                                         .then(Mono.from(connection.createStatement(LEAVE_ROOM_SQL)
-                                                 .bind(0, oldRoomId).bind(1, username).execute()))
-                                         .then(Mono.from(connection.createStatement(ENTER_ROOM_SQL)
-                                                 .bind(0, roomId).bind(1, username).execute()))
-                                         .then((Mono<Void>)connection.commitTransaction())
-                                         .doFinally(signalType -> ((Mono<Void>)connection.close()).subscribe()));
-                    }else{
+        return redisTemplate.opsForHash().get(USER2ROOM, username)
+                .switchIfEmpty(initRoomIdMono)
+                .map(i -> Integer.parseInt((String) i, 10))
+                .flatMap(oldRoomId -> {
+                    if (oldRoomId == roomId) {
+                        return trueMono;
+                    }
+                    Mono<?> afterMysql;
+                    if (oldRoomId != -1) {
+                        afterMysql = connectionMysql.flatMap(connection ->
+                                ((Mono<Void>) connection.beginTransaction())
+                                        .then(Mono.from(connection.createStatement(LEAVE_ROOM_SQL)
+                                                .bind(0, oldRoomId).bind(1, username).execute()))
+                                        .then(Mono.from(connection.createStatement(ENTER_ROOM_SQL)
+                                                .bind(0, roomId).bind(1, username).execute()))
+                                        .then((Mono<Void>) connection.commitTransaction())
+                                        .doFinally(signalType -> ((Mono<Void>) connection.close()).subscribe()));
+                    } else {
                         afterMysql = connectionMysql.flatMap(connection ->
                                 Mono.from(connection.createStatement(ENTER_ROOM_SQL)
                                         .bind(0, roomId).bind(1, username).execute())
-                                        .doFinally(signalType -> ((Mono<Void>)connection.close()).subscribe()));
+                                        .doFinally(signalType -> ((Mono<Void>) connection.close()).subscribe()));
                     }
                     return afterMysql.then(redisTemplate.opsForHash().put(USER2ROOM, username, Integer.toString(roomId)))
                             .then(trueMono)
@@ -79,36 +80,51 @@ public class RoomRepository {
                 });
     }
 
-    public Mono<Boolean> leaveRoom(String username,int roomId){
-        return Mono.just(true);
+    public Mono<Boolean> leaveRoom(String username) {
+        return redisTemplate.opsForHash().get(USER2ROOM, username)
+                .switchIfEmpty(initRoomIdMono)
+                .map(i -> Integer.parseInt((String) i, 10))
+                .flatMap(oldRoomId -> {
+                    if (oldRoomId == -1) {
+                        return falseMono;
+                    } else {
+                        return connectionMysql.flatMap(connection ->
+                                Mono.from(connection.createStatement(LEAVE_ROOM_SQL)
+                                        .bind(0, oldRoomId).bind(1, username).execute())
+                                        .then(redisTemplate.opsForHash().put(USER2ROOM, username, "-1"))
+                                        .then(trueMono)
+                                        .onErrorResume(throwable -> falseMono)
+                                        .doFinally(signalType -> ((Mono<Void>) connection.close()).subscribe()));
+                    }
+                });
     }
 
-    public Mono<String> getRoomInfo(int roomId){
+    public Mono<String> getRoomInfo(int roomId) {
         return connectionMysql.flatMap(connection ->
                 Mono.from(connection.createStatement(GET_ROOM_INFO_SQL)
                         .bind(0, roomId).execute())
                         .flatMap(result -> Mono.from(result.map((row, rowMetadata) -> row.get(0, String.class))))
-                        .doFinally(signalType -> ((Mono<Void>)connection.close()).subscribe()));
+                        .doFinally(signalType -> ((Mono<Void>) connection.close()).subscribe()));
     }
 
-    public Mono<List<String>> getUsers(int roomId){
+    public Mono<List<String>> getUsers(int roomId) {
         return connectionMysql.flatMap(connection ->
                 Flux.from(connection.createStatement(GET_USERS_SQL)
                         .bind(0, roomId).execute())
                         .flatMap(result -> Mono.from(result.map((row, rowMetadata) -> row.get(0, String.class))))
                         .collectList()
-                        .doFinally(signalType -> ((Mono<Void>)connection.close()).subscribe()));
+                        .doFinally(signalType -> ((Mono<Void>) connection.close()).subscribe()));
     }
 
-    public Mono<List<RoomBO>> getRooms(PageBO pageBO){
+    public Mono<List<RoomBO>> getRooms(PageBO pageBO) {
         return connectionMysql.flatMap(connection ->
                 Flux.from(connection.createStatement(GET_ROOMS_SQL)
-                        .bind(0, pageBO.getPageIndex()* pageBO.getPageSize())
+                        .bind(0, pageBO.getPageIndex() * pageBO.getPageSize())
                         .bind(1, pageBO.getPageSize()).execute())
                         .flatMap(result -> result.map((row, rowMetadata) ->
-                                new RoomBO(row.get(1,String.class),row.get(0,Integer.class).toString())))
+                                new RoomBO(row.get(1, String.class), row.get(0, Integer.class).toString())))
                         .collectList()
-                        .doFinally(signalType -> ((Mono<Void>)connection.close()).subscribe()));
+                        .doFinally(signalType -> ((Mono<Void>) connection.close()).subscribe()));
     }
 
 }
--- v1/src/main/java/com/example/achatroom/handler/RoomHandler.java
+++ v1/src/main/java/com/example/achatroom/handler/RoomHandler.java
@@ -28,51 +28,56 @@ public class RoomHandler {
     }
 
     @NotNull
-    public Mono<ServerResponse> createRoom(ServerRequest serverRequest){
+    public Mono<ServerResponse> createRoom(ServerRequest serverRequest) {
         return serverRequest.bodyToMono(RoomNameBO.class)
-                .flatMap(roomNameBO -> okResponseBuilder.body(roomRepository.createRoom(roomNameBO.getName()),String.class))
+                .flatMap(roomNameBO -> okResponseBuilder.body(roomRepository.createRoom(roomNameBO.getName()), String.class))
                 .switchIfEmpty(badResponse);
     }
 
     @NotNull
-    public Mono<ServerResponse> enterRoom(ServerRequest serverRequest){
+    public Mono<ServerResponse> enterRoom(ServerRequest serverRequest) {
         List<String> list = serverRequest.headers().header("name");
-        if(list.isEmpty()){
+        if (list.isEmpty()) {
             return badResponse;
         }
         String username = list.get(0);
-        int roomId=Integer.parseInt(serverRequest.pathVariable("roomId"),10);
-        return roomRepository.enterRoom(username,roomId).flatMap(aBool -> aBool?okResponse:badResponse);
+        int roomId = Integer.parseInt(serverRequest.pathVariable("roomId"), 10);
+        return roomRepository.enterRoom(username, roomId).flatMap(aBool -> aBool ? okResponse : badResponse);
     }
 
     @NotNull
-    public Mono<ServerResponse> leaveRoom(ServerRequest serverRequest){
-        return ServerResponse.ok().body(Mono.just("null"),String.class);
+    public Mono<ServerResponse> leaveRoom(ServerRequest serverRequest) {
+        List<String> list = serverRequest.headers().header("name");
+        if (list.isEmpty()) {
+            return badResponse;
+        }
+        String username = list.get(0);
+        return roomRepository.leaveRoom(username).flatMap(aBool -> aBool ? okResponse : badResponse);
     }
 
     @NotNull
-    public Mono<ServerResponse> getRoomInfo(ServerRequest serverRequest){
-        Mono<String> name=roomRepository.getRoomInfo(Integer.parseInt(
+    public Mono<ServerResponse> getRoomInfo(ServerRequest serverRequest) {
+        Mono<String> name = roomRepository.getRoomInfo(Integer.parseInt(
                 serverRequest.pathVariable("roomId"), 10));
         return name.flatMap(s -> okResponseBuilder.body(name, String.class))
                 .switchIfEmpty(badResponse);
     }
 
     @NotNull
-    public Mono<ServerResponse> getUsers(ServerRequest serverRequest){
+    public Mono<ServerResponse> getUsers(ServerRequest serverRequest) {
         return roomRepository.getUsers(Integer.parseInt(serverRequest.pathVariable("roomId"), 10))
-                .flatMap(strings -> strings.size()!=0
-                        ?okResponseBuilder.body(Flux.fromIterable(strings),String.class)
-                        :badResponse);
+                .flatMap(strings -> strings.size() != 0
+                        ? okResponseBuilder.body(Flux.fromIterable(strings), String.class)
+                        : badResponse);
     }
 
     @NotNull
-    public Mono<ServerResponse> getRooms(ServerRequest serverRequest){
+    public Mono<ServerResponse> getRooms(ServerRequest serverRequest) {
         return serverRequest.bodyToMono(PageBO.class)
-                .flatMap(pageBO -> pageBO.getPageIndex()<0 ?badResponse
-                        :roomRepository.getRooms(pageBO).flatMap(roomBOList -> roomBOList.size()!=0
-                            ?okResponseBuilder.body(Flux.fromIterable(roomBOList),RoomBO.class)
-                            :badResponse));
+                .flatMap(pageBO -> pageBO.getPageIndex() < 0 ? badResponse
+                        : roomRepository.getRooms(pageBO).flatMap(roomBOList -> roomBOList.size() != 0
+                        ? okResponseBuilder.body(Flux.fromIterable(roomBOList), RoomBO.class)
+                        : badResponse));
 //        reactor的反人类之处,flux/mono对象不会被修改，但里面的数据流留出去之后就没了，然后我判断一下有没有元素，然后里面的元素也没了
 //        return rooms.hasElements().flatMap(aBool -> aBool?okResponseBuilder.body(rooms,RoomBO.class):badResponse);
     }
